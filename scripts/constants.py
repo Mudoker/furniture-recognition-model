d4183fd743b4e574d4a3a519a26aff6d190549b6
Quốc Đoàn <huuquoc7603@gmail.com>
2024-05-20 17:13:15 +0700

    feat: add more constants for skipping training process

---
+++ scripts/constants.py
@@ -0,0 +1,19 @@
+# Styling constants
+BULLET_POINT = ">>>"
+
+# Environment constants
+IS_COLAB = False
+
+# Constatns for different environments
+COLAB_PATH = "/content"
+LOCAL_PATH = "../.."
+
+# Relative paths to the data directories
+DATA_DIR = "data"
+RAW_DATA_DIR = f"{DATA_DIR}/raw/Furniture_Data/Furniture_Data"
+PROCESSED_DATA_DIR = f"{DATA_DIR}/processed"
+TEST_DATA_DIR = f"{DATA_DIR}/test"
+
+# System Constants
+IS_TRAIN = False
+IS_COLAB_UNZIP = False
